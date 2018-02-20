022e77660fc0d2ccf0e0471f585ebd998bc13e4b
YOUNG\Young <illusioner12@gmail.com>
2018-02-20 22:54:16 +0900

    realParser API calling update
    # json 처리

--- RealParser/realParserAPI.py
+++ RealParser/realParserAPI.py
@@ -1,80 +1,68 @@
 import requests
 import time
 import json
-from urllib.parse import quote
-
-
-
-
 
 class RealParser :
     params = {
         'key': '28e650e7-f611-4e98-a5ac-4f481f77922f',
     }
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+    }
 
     def getParsingTag(self, text) :
-        textList = text.split(".")
-        parsingList = []
+        count = 0
+        self.params['text'] = text
 
-        for i in range(len(textList) - 1) :
-            textList[i] += "."
-
-        for text in textList :
-            if text.strip() == "" : continue
-            while True :
-                try:
-                    parsingData = requests.post(url="http://iip.hanbat.ac.kr/RealParser/parsingApi?text=" + quote(text),
-                                                data=self.params)
-                except:
-                    time.sleep(2)
-                    continue
-                parsingList.append(parsingData.json())
-                break
-
-        parsingJsonData = parsingList[0]
-
-        for i in range(1, len(parsingList)) :
-            i_json = parsingList[i]
-            parsingJsonData['total_count'] += i_json.get('total_count')
-            pjList = parsingJsonData.get("result_list")
-            pjList.extend(i_json.get("result_list"))
-
-        return parsingJsonData
-
-
-    def getParsingLSU(self, text, isNone=False):
         while True :
+            if count > 5 :
+                break
             try:
-                parsingData = requests.post(url="http://iip.hanbat.ac.kr/RealParser/parsingLSU?text=" + quote(text),
-                                            data=self.params)
+                parsingData = requests.post(url="http://iip.hanbat.ac.kr/RealParser/parsingApi",
+                                            data=self.params, headers=self.headers).text
+                count += 1
+                break
             except:
                 time.sleep(2)
                 continue
-            parsingData = parsingData.text.strip()
 
-            if isNone and parsingData == "" :
-                print("parsing results is None !!!")
+        parsingData = json.loads(parsingData)
+        return parsingData
+
+
+    def getParsingLSU(self, text):
+        count = 0
+        self.params['text'] = text
+
+        while True:
+            if count > 5:
+                break
+            try:
+                parsingData = requests.post(url="http://iip.hanbat.ac.kr/RealParser/parsingLSU",
+                                            data=self.params, headers=self.headers).text
+                count += 1
+                break
+            except:
+                time.sleep(2)
                 continue
-
-            break
 
         return parsingData
 
 
     def getParsingWord(self, text):
-        textList = text.split(".")
-        textList = [i for i in textList if i != ""]
-        resultsList = ""
+        count = 0
+        self.params['text'] = text
 
-        for t in textList :
-            while True :
-                try:
-                    parsingData = requests.post(url="http://iip.hanbat.ac.kr/RealParser/parsing?text=" + quote(t),
-                                                data=self.params)
-                except:
-                    time.sleep(2)
-                    continue
-                resultsList += parsingData.text.strip()
+        while True:
+            if count > 5:
                 break
+            try:
+                parsingData = requests.post(url="http://iip.hanbat.ac.kr/RealParser/parsing",
+                                            data=self.params, headers=self.headers).text
+                count += 1
+                break
+            except:
+                time.sleep(2)
+                continue
 
-        return resultsList
+        return parsingData
